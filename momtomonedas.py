61d26e94becfc2b79f0e0fd27a68ad6405220dbf
lpizarro2391 <lourdespizarro@gmail.com>
2020-04-11 23:18:22 -0600

    innecesaior semicolon

---
+++ momtomonedas.py
@@ -0,0 +1,3 @@
+cripto1=(str(int('ingrese la cantidad cripto1')))
+cripto2=(str(int('ingrese la cantidad cripto2')))
+cripto3=(str(int('ingrese la cantidad cripto3')))
--- saldophone.py
+++ saldophone.py
@@ -1,15 +1,15 @@
 BITCOIN=7630.80
 DASHUSD=315.56
 LTCUSD=120.84
-moneda=input("ingrese la moneda a utilizar (BTC, DASH o LTC):");
+moneda=input("ingrese la moneda a utilizar (BTC, DASH o LTC):")
 if moneda=="BTC" or moneda=="DASH" or moneda=="LTC":
-    cantidad=float(input("ingrese la cantidad a utilizar:"));
+    cantidad=float(input("ingrese la cantidad a utilizar:"))
     if moneda=="BTC":
         monto=cantidad * BITCOIN;
     elif moneda=="DASH":
             monto=cantidad * DASHUSD;
     else:
             monto=cantidad * LTCUSD;
-    print("la cantidad de USD recargado fue de: "+str(monto));
+    print("la cantidad de USD recargado fue de: "+str(monto))
 else:
-    print("Error solo se permite utilizar BTC, DASH o LTC");
+    print("Error solo se permite utilizar BTC, DASH o LTC")
