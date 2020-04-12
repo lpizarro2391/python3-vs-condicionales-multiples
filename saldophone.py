747f40dc0bfca63216260d1fcd6f9d9927196dbd
lpizarro2391 <lourdespizarro@gmail.com>
2020-04-12 00:28:25 -0600

    correcion

--- saldophone.py
+++ saldophone.py
@@ -5,11 +5,11 @@ moneda=input("ingrese la moneda a utilizar (BTC, DASH o LTC):")
 if moneda=="BTC" or moneda=="DASH" or moneda=="LTC":
     cantidad=float(input("ingrese la cantidad a utilizar:"))
     if moneda=="BTC":
-        monto=cantidad * BITCOIN;
+        monto=cantidad * BITCOIN
     elif moneda=="DASH":
-            monto=cantidad * DASHUSD;
+            monto=cantidad * DASHUSD
     else:
-            monto=cantidad * LTCUSD;
+            monto=cantidad * LTCUSD
     print("la cantidad de USD recargado fue de: "+str(monto))
 else:
     print("Error solo se permite utilizar BTC, DASH o LTC")
